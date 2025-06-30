6364f24d1cc493f12641555861331cb189a4ddc7
Shreyas <sas27@illinois.edu>
2025-06-30 00:45:37 -0500

    "more"

--- scripts.js
+++ scripts.js
@@ -4,15 +4,21 @@ function showSection(sectionId) {
     sections.forEach(section => {
         section.style.display = 'none';
     });
-
     // Show the selected section
     const activeSection = document.getElementById(sectionId);
     if (activeSection) {
         activeSection.style.display = 'block';
     }
 }
-
 // Show the 'About Me' section by default
 document.addEventListener('DOMContentLoaded', () => {
     showSection('about');
 });
+
+const toggleButton = document.getElementById('theme-toggle');
+
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+});
+
+
